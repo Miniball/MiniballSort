6f37b85dbf77b098ad87177ba71fa7f01fe64e59
Liam Gaffney <lpgaff@gmail.com>
2023-07-22 13:51:15 +0200

    bug fixes for the DataSpy read logic and typos in the test for if the calibration/reaction files exist

--- mb_sort.cc
+++ mb_sort.cc
@@ -197,23 +197,26 @@ void* monitor_run( void* ptr ){
 				}
 
 				// Keep reading until we have all the data
+				// This could be multi-threaded to process data and go back to read more
 				int block_ctr = 0;
-				while( spy_length ){
+				long byte_ctr = 0;
+				int poll_ctr = 0;
+				while( block_ctr < 200 && poll_ctr < 1000 ){
 				
 					//std::cout << "Got some data from DataSpy" << std::endl;
 					nblocks = conv_midas_mon->ConvertBlock( (char*)buffer, 0 );
 					block_ctr += nblocks;
 
-					// Stop after so long
-					if( block_ctr > 50 ) break;
-
 					// Read a new block
-					//gSystem->Sleep( 10 ); // wait 10 ms
+					gSystem->Sleep( 1 ); // wait 1 ms between each read
 					spy_length = myspy.Read( file_id, (char*)buffer, calfiles->myset->GetBlockSize() );
+					
+					byte_ctr += spy_length;
+					poll_ctr++;
 
 				}
 				
-				std::cout << "Got " << spy_length << " bytes of data from DataSpy" << std::endl;
+				std::cout << "Got " << byte_ctr << " bytes of data from DataSpy" << std::endl;
 
 				// Sort the packets we just got, then do the rest of the analysis
 				conv_midas_mon->SortTree();
@@ -625,7 +628,7 @@ int main( int argc, char *argv[] ){
 		
 	}
 	
-	else if( mon_time > 0 && input_names.size() == 1 ) {
+	else if( mon_time >= 0 && input_names.size() == 1 ) {
 		
 		flag_monitor = true;
 		std::cout << "Running sort in a loop every " << mon_time;
@@ -633,7 +636,7 @@ int main( int argc, char *argv[] ){
 		
 	}
 	
-	else if( mon_time > 0 && input_names.size() != 1 ) {
+	else if( mon_time >= 0 && input_names.size() != 1 ) {
 		
 		flag_monitor = false;
 		std::cout << "Cannot monitor multiple input files, switching to normal mode" << std::endl;
@@ -689,7 +692,7 @@ int main( int argc, char *argv[] ){
 			
 			std::cout << name_cal_file << " does not exist.";
 			std::cout << " Using defaults" << std::endl;
-			name_set_file = "dummy";
+			name_cal_file = "dummy";
 
 		}
 		
@@ -719,7 +722,7 @@ int main( int argc, char *argv[] ){
 			
 			std::cout << name_react_file << " does not exist.";
 			std::cout << " Using defaults" << std::endl;
-			name_set_file = "dummy";
+			name_react_file = "dummy";
 
 		}
 		
