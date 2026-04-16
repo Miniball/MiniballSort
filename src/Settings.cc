b2dbb0dc29d8f8338698b6bee4dfb12d449097c3
Carlotta Porzio <48970374+carlottaporzio@users.noreply.github.com>
2026-04-16 15:30:00 +0200

    Update src/Settings.cc
    Co-authored-by: Liam Gaffney <lpgaff@gmail.com>

--- src/Settings.cc
+++ src/Settings.cc
@@ -272,7 +272,7 @@ void MiniballSettings::ReadSettings() {
 	bufferpart_reject	= config->GetValue( "BufferPartRejection", false );
 
 	// CD calibrator settings
-	cd_calibrator_max_energy = config->GetValue( "CDCalibratorMaxEn", 2000e3 );
+	cd_calibrator_max_energy = config->GetValue( "CDCalibrator.MaxEnergy", 2000e3 );
 	
 	// Electronics mapping
 	// Will depend on whether we have FEBEX or DGF/ADC combo
